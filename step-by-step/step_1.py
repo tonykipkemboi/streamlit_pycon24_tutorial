40ca10673b23c13639b1de6b19f992e318a93137
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2024-05-11 10:37:14 -0400

    Update step_1.py

--- step-by-step/step_1.py
+++ step-by-step/step_1.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import openai
-import llama_index
+from llama_index.llms.openai import OpenAI
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
 
 openai.api_key = st.secrets.openai_api_key
 st.title("Chat with the Streamlit docs, powered by LlamaIndex")
