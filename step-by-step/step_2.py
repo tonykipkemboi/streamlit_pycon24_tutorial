b0bb6cfd3dfb9e9ccc3e434c9e9cb2b82ee926b8
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2024-05-11 10:42:14 -0400

    Create step_2.py

---
+++ step-by-step/step_2.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import openai
+from llama_index.llms.openai import OpenAI
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
+
+openai.api_key = st.secrets.openai_api_key
+st.title("Chat with the Streamlit docs, powered by LlamaIndex")
+
+reader = SimpleDirectoryReader(input_dir="./docs", recursive=True)
+docs = reader.load_data()
