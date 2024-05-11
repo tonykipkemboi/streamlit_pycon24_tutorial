1fac43525eb79f8e5fc06a9ee7c34b6b8a02d8ba
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2024-05-11 10:52:09 -0400

    Create step_4.py

---
+++ step-by-step/step_4.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import openai
+from llama_index.llms.openai import OpenAI
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
+
+openai.api_key = st.secrets.openai_api_key
+st.title("Chat with the Streamlit docs, powered by LlamaIndex")
+
+reader = SimpleDirectoryReader(input_dir="./docs", recursive=True)
+docs = reader.load_data()
+
+Settings.llm = OpenAI(
+    model="gpt-3.5-turbo",
+    temperature=0.2,
+    system_prompt="""You are an expert on 
+    the Streamlit Python library and your 
+    job is to answer technical questions. 
+    Assume that all questions are related 
+    to the Streamlit Python library. Keep 
+    your answers technical and based on 
+    facts – do not hallucinate features."""
+)
+
+index = VectorStoreIndex.from_documents(docs)
+
+chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
