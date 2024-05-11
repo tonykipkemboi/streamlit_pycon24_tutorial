ecfe387af3618a2290aacd6d8f14b8ad738f9d49
Tony Kipkemboi <iamtonykipkemboi@gmail.com>
2024-05-11 16:45:00 -0400

    Update 02_💬_Chat_with_the_Streamlit_docs.py
    refactor

--- pages/02_💬_Chat_with_the_Streamlit_docs.py
+++ pages/02_💬_Chat_with_the_Streamlit_docs.py
@@ -1,15 +1,19 @@
 import streamlit as st
 import openai
 from llama_index.llms.openai import OpenAI
-from llama_index.core import VectorStoreIndex, Document, SimpleDirectoryReader, Settings
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
 
-openai.api_key = st.secrets.openai_api_key
+openai.api_key = st.secrets.OPENAI_API_KEY
 st.title("Chat with the Streamlit docs, powered by LlamaIndex ")
 
-if "messages" not in st.session_state.keys(): # Initialize the chat messages history
+if "messages" not in st.session_state.keys():  # Initialize the chat messages history
     st.session_state.messages = [
-        {"role": "assistant", "content": "Ask me a question about Streamlit's open-source Python library!"}
+        {
+            "role": "assistant",
+            "content": "Ask me a question about Streamlit's open-source Python library!",
+        }
     ]
+
 
 @st.cache_resource()
 def load_data():
@@ -24,20 +28,25 @@ def load_data():
         Assume that all questions are related 
         to the Streamlit Python library. Keep 
         your answers technical and based on 
-        facts – do not hallucinate features."""
-        )
+        facts – do not hallucinate features.""",
+    )
     index = VectorStoreIndex.from_documents(docs)
     return index
 
+
 index = load_data()
 
-if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
-    st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True, streaming=True)
+if "chat_engine" not in st.session_state.keys():  # Initialize the chat engine
+    st.session_state.chat_engine = index.as_chat_engine(
+        chat_mode="condense_question", verbose=True, streaming=True
+    )
 
-if prompt := st.chat_input("Ask a question"): # Prompt for user input and save to chat history
+if prompt := st.chat_input(
+    "Ask a question"
+):  # Prompt for user input and save to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
 
-for message in st.session_state.messages: # Write message history to UI
+for message in st.session_state.messages:  # Write message history to UI
     with st.chat_message(message["role"]):
         st.write(message["content"])
 
@@ -47,4 +56,5 @@ if st.session_state.messages[-1]["role"] != "assistant":
         response_stream = st.session_state.chat_engine.stream_chat(prompt)
         st.write_stream(response_stream.response_gen)
         message = {"role": "assistant", "content": response_stream.response}
-        st.session_state.messages.append(message) # Add response to message history
+        # Add response to message history
+        st.session_state.messages.append(message)
