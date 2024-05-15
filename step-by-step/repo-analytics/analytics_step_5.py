c6819625ea7873b2998bfe31121a9d7b6d098792
Tony Kipkemboi <iamtonykipkemboi@gmail.com>
2024-05-15 12:07:50 -0400

    Update analytics_step_5.py

--- step-by-step/repo-analytics/analytics_step_5.py
+++ step-by-step/repo-analytics/analytics_step_5.py
@@ -3,7 +3,6 @@ import pandas as pd
 import plotly.express as px
 from pandas import DataFrame
 
-
 @st.cache_data
 def load_data(filepath: str) -> DataFrame:
     """
@@ -95,3 +94,6 @@ def main():
                 ["cumulative_additions", "cumulative_deletions"]
             ]
         )
+
+if __name__ == "__main__":
+    main()
